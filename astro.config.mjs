dee5e50746da71a75877c6dcc161d34f03d79be0
MarcellusStack <MarcellusStack@gmail.com>
2025-08-20 13:08:10 +0200

    feat: update dependencies and integrate Tabler icons
    - Added @astrojs/react and @tabler/icons-react to dependencies.
    - Removed favicon.svg and updated BaseLayout.astro to use favicon.ico.
    - Replaced icon font classes with Tabler icons in index.astro.
    - Updated tsconfig.json to support React JSX syntax.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,48 +7,46 @@ import tailwindcss from "@tailwindcss/vite";
 import clerk from "@clerk/astro";
 import { deDE } from "@clerk/localizations";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.beachdude.de",
-  integrations: [
-    clerk({
-      localization: deDE,
-      appearance: {
-        elements: {
-          button: {
-            backgroundColor: "#002d55",
-            color: "#fff",
-          },
+  integrations: [clerk({
+    localization: deDE,
+    appearance: {
+      elements: {
+        button: {
+          backgroundColor: "#002d55",
+          color: "#fff",
         },
       },
-    }),
-    starlight({
-      title: "BeachDude",
-      customCss: [
-        // Path to your Tailwind base styles:
-        "./src/styles/global.css",
-      ],
-      social: [],
-      logo: {
-        light: "./src/assets/logo.png",
-        dark: "./src/assets/logo.png",
+    },
+  }), starlight({
+    title: "BeachDude",
+    customCss: [
+      // Path to your Tailwind base styles:
+      "./src/styles/global.css",
+    ],
+    social: [],
+    logo: {
+      light: "./src/assets/logo.png",
+      dark: "./src/assets/logo.png",
+    },
+    sidebar: [
+      {
+        label: "Guides",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: "Example Guide", slug: "docs/guides/example" },
+        ],
       },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "docs/guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "docs/reference" },
-        },
-      ],
-    }),
-    sitemap(),
-  ],
+      {
+        label: "Reference",
+        autogenerate: { directory: "docs/reference" },
+      },
+    ],
+  }), sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
